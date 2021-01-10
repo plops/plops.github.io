94b0b2a34fd654222bea9ae048909c4375903599
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-01-10 17:01:11 +0100

    add url

--- gen00.lisp
+++ gen00.lisp
@@ -25,13 +25,131 @@
 		       (pathname-name item))))))))
    s))
 
-
+(defparameter *graph*
+  `(
+    main   sexpr
+	   main   sdr
+	   sdr  pluto
+	   sdr  sar
+	   sexpr  (cpp2 cl-cpp-generator2)
+	   sexpr  python
+	   sexpr  kotlin 
+	   sexpr  rust 
+	   sexpr  elixir
+	   sexpr  go
+	   sexpr  js
+	   sexpr  matlab
+	   sexpr  swift
+	   python   finance
+	   rust  cuda
+	   rust  pluto
+	   rust  uprof
+	   cpp2  optix
+	   main  android
+	   cpp2  wasm
+	   cpp2  cuda
+	   cpp2  vulkan
+	   cpp2  script
+	   script lua_in_cpp
+	   cpp2  tbb
+	   cpp2  embed
+	   embed  pluto
+	   embed  psoc
+	   embed  stm32
+	   cpp2  sar
+	   cpp2  gui
+	   gui  gtk
+	   gui  qt
+	   gui  fltk
+	   gui  imgui
+	   gui  wx
+	   cpp2  asio
+	   cpp2  script
+	   script pybind11
+	   pybind11 python_in_cpp
+	   cpp2  termimg
+	   elixir  phx
+	   python  ai
+	   python  pyvulkan
+	   python  autoweb
+	   autoweb  helium
+	   autoweb  selenium
+	   python  pygui
+	   pygui  pywx
+	   pygui  gtk3
+	   pygui  pyqt
+	   pyqt webkit
+	   pygui  glumpy
+	   pygui  tkinter
+	   pygui  visdom
+	   qt  trellis
+	   qt  webengine
+	   python  waveguide
+	   ai  megatron
+	   ai  fastai
+	   ai  fastai2
+	   fastai2  nlp
+	   ai  pytorch
+	   python  numba
+	   python  pycuda
+	   python  cupy
+	   python  dask
+	   js  electron
+	   kotlin  kot_opengles
+	   kotlin  sensors
+	   kotlin  camera
+	   kotlin  renderscript
+	   kotlin  grpc
+	   kotlin  gps
+	   kotlin  key_secure
+	   kotlin  compress
+	   kotlin  encrypt
+	   swift  tensorflow
+	   go  gogui
+	   gogui fayne
+	   go  gogrpc
+	   go  concur_chan))
 
 ;; pip install --user graphviz
 (ql:quickload "cl-py-generator")
 (in-package :cl-py-generator)
-(write-source "/home/martin/stage/plops.github.io/gen_graphviz"
-	      `(do0
-		(imports (graphviz))
-		(setf dot (Digraph :comment (string "projects")))
-		))
+
+
+(let ((nodes nil)
+      (links nil))
+  (loop for e in *graph* do
+    (setf nodes (adjoin (if (listp e)
+			    (first e)
+			    e) nodes)))
+  (loop for e in *graph* do
+    (when (listp e)
+      (push e links)))
+  (defparameter *links* links)
+
+  (write-source "/home/martin/stage/plops.github.io/gen_graphviz"
+		`(do0
+		  (imports (graphviz))
+		  (setf g (graphviz.Digraph :comment (string "projects")))
+		  ,@(loop for e in nodes
+			  collect
+			  (let ((o (cadr (assoc e links))))
+			    (if o
+			     `(g.node (string ,e)
+				      :URL
+				   
+				      (string ,(format nil "https://github.com/plops/~a" o)))
+			     `(g.node (string ,e)
+				      ))))
+		  ,@(loop for (a b) on *graph* by #'cddr
+			  collect
+			  (let ((a1 (if (listp a)
+					(first a)
+					a))
+				(b1 (if (listp b)
+					(first b)
+					b)))
+			    `(g.edge (string ,a1)
+				     (string ,b1))))
+		  (print g.source))))
+
+
